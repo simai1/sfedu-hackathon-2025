62adcacd75b29767e0c97d411c148eac10999c43
simai1 <mksim3uchnev@gmail.com>
2025-12-05 23:49:50 +0300

    fix

--- back/app/adapters/rest/v1/routes/auth.py
+++ back/app/adapters/rest/v1/routes/auth.py
@@ -3,24 +3,32 @@ from app.composites.auth_composite import get_controller
 from app.adapters.rest.v1.controllers.auth import AuthController
 from app.domains.user import CreateUser, ValidateUser
 from app.domains.auth import AuthUser
+from pydantic import BaseModel
 
 router = APIRouter()
 
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
 @router.post("/login", response_model=AuthUser)
 async def post_login(
-    email: str = Form(...),
-    password: str = Form(...),
+    login_req: LoginRequest,
     controller: AuthController = Depends(get_controller),
 ):
-    validate_user = ValidateUser(email=email, password=password)
+    validate_user = ValidateUser(email=login_req.email, password=login_req.password)
     return await controller.login(validate_user)
+
+class RegisterRequest(BaseModel):
+    email: str
+    password: str
+    name: str
+
 
 @router.post("/register", response_model=AuthUser)
 async def register(
-    email: str = Form(...),
-    password: str = Form(...),
-    name: str = Form(...),
+    register_req: RegisterRequest,
     controller: AuthController = Depends(get_controller),
 ):
-    create_user = CreateUser(email=email, password=password, name=name)
+    create_user = CreateUser(email=register_req.email, password=register_req.password, name=register_req.name)
     return await controller.register(create_user)
